1c402deb18bc07dc6fb712698b321dda38e25887
George <george@betterde.com>
2018-09-08 13:26:46 +0800

    Modify api routes

--- routes/v1.php
+++ routes/v1.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'staff', 'namespace' => 'Staff'], function () {
+    Route::apiResource('project', 'ProjectController');
 });
