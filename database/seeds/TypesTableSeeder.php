a44b7074ec0c138728593d6cf27fb591c76eb831
George <george@betterde.com>
2018-09-30 22:40:42 +0800

    create type data generator

---
+++ database/seeds/TypesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = [
+            [
+
+            ]
+        ];
+    }
+}
