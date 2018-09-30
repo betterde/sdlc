12b61107cf49e93662405f9e9faae4e135a61f39
George <george@betterde.com>
2018-09-30 22:03:59 +0800

    create type model and migration file

---
+++ app/Models/Type.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 项目类型数据模型
+ *
+ * Date: 2018/9/21
+ * @author George
+ * @package App\Models
+ */
+class Type extends Model
+{
+    //
+}
