2818a35f2e4950bf43393e71d8b0d13a9485d83d
George <george@betterde.com>
2018-10-13 11:27:20 +0800

    create environment model and migration file

---
+++ database/migrations/2018_10_12_162405_create_environments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEnvironmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('environments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->comment('名称');
+            $table->json('configuration')->comment('配置信息');
+            $table->unsignedInteger('project_id')->index()->comment('项目ID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('environments');
+    }
+}
