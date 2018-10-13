fbc19db540143cc4b9686e3037b0c5cb541bd6b9
George <george@betterde.com>
2018-10-13 11:32:14 +0800

    create service model and migration file

---
+++ database/migrations/2018_10_12_164329_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->comment('名称');
+            $table->string('description')->nullable()->comment('描述');
+            $table->json('optinos')->comment('选项');
+            $table->timestamps();
+        });
+        table('services', '服务数据表');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
