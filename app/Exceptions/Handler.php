906fadd6657578d74cc9fc2a7bf9a68fcaa6cbef
George <george@betterde.com>
2018-10-18 23:50:37 +0800

    add validation exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +51,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+    	if ($exception instanceof ValidationException) {
+			return failed(array_first(array_collapse($exception->errors())), 422);
+		}
         return parent::render($request, $exception);
     }
 
