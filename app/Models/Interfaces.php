4015d0eed8eab973d0db19cefdd3c72f6a2c9c39
George <george@betterde.com>
2018-10-27 15:10:09 +0800

    create interfaces model and migration file

---
+++ app/Models/Interfaces.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 项目接口数据模型
+ *
+ * Date: 2018/10/27
+ * @author George
+ * @package App\Models
+ */
+class Interfaces extends Model
+{
+    //
+}
