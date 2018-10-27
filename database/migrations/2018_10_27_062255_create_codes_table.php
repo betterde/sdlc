1f008c68c833f81a5299f12ac801a52272b639f5
George <george@betterde.com>
2018-10-27 15:12:16 +0800

    create code model and migration file

---
+++ database/migrations/2018_10_27_062255_create_codes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('group_id')->index()->comment('分组ID');
+            $table->integer('number')->index()->comment('状态码');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+        table('codes', '状态码数据表');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
