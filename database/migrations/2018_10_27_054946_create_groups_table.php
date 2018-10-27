eca60ed39dcd7a9d98c470828f36e9d54537e741
George <george@betterde.com>
2018-10-27 15:12:38 +0800

    create group model and migration file

---
+++ database/migrations/2018_10_27_054946_create_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('scene')->comment('场景');
+            $table->unsignedInteger('parent_id')->index()->comment('父级ID');
+            $table->unsignedInteger('name')->index()->comment('父级ID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
