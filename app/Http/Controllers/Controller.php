aa72303fb5d7df8631158ccfc03090bf1ead2ec5
George <george@betterde.com>
2018-10-28 21:58:07 +0800

    add comment to controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,13 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * 基础控制器
+ *
+ * Date: 2018/10/28
+ * @author George
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
