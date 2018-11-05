405414c2d39ed055f6e26861132178c3b103b7c2
George <george@betterde.com>
2018-11-05 23:41:44 +0800

    create group controller

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Group;
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Argument  $argument
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Group $argument)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Group  $argument
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Group $argument)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Group  $argument
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Group $argument)
+    {
+        //
+    }
+}
