665101405561d95f3ada1443706ade664711adae
George <george@betterde.com>
2019-07-26 23:00:35 +0800

    # This is a combination of 4 commits. # This is the 1st commit message:
    upgrade dependencies
    
    # This is the commit message #2:
    
    modify kernel load middleware
    
    upgrade dependencies
    
    # This is the commit message #3:
    
    upgrade dependence
    
    # This is the commit message #4:
    
    modify kernel load middleware

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -77,6 +78,10 @@ class Handler extends ExceptionHandler
     	if ($exception instanceof NotFoundHttpException) {
     	    return notFound();
         }
+
+    	if ($exception instanceof ThrottleRequestsException) {
+    		return failed("请求过于频繁", 400);
+		}
 
         return parent::render($request, $exception);
     }
